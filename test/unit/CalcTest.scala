83bff6b9167009d5af6edae057dbfb3e70719533
zenon <chrisduraj@gmail.com>
2012-05-03 12:34:42 +0100

    test

---
+++ test/unit/CalcTest.scala
@@ -0,0 +1,13 @@
+package unit
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zenon
+ * Date: 03/05/12
+ * Time: 12:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+class CalcTest {
+
+}
